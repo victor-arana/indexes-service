54f54da49ee2e314cef6da590c24c7d3978ff25c
victor-arana <victor_arana@hotmail.com>
2020-08-28 23:14:08 -0500

    :wrench: initialization, initialize repository

--- src/main/java/no/data/indexes/IndexesServiceApplication.java
+++ src/main/java/no/data/indexes/IndexesServiceApplication.java
@@ -1,13 +1,25 @@
 package no.data.indexes;
 
+import no.data.indexes.dao.IndexRepository;
+import no.data.indexes.model.Index;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IndexesServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndexesServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(IndexRepository repository){
+		return args -> {
+			repository.save(new Index(1,"Region 1", 20.0));
+			repository.save(new Index(2,"Region 2", 80.0));
+		};
 	}
 
 }
