add4e97a3dfda076ec8a66a4739d2ac303cabb0d
victor-arana <victor_arana@hotmail.com>
2020-08-28 23:19:57 -0500

    wrench: cross-origin, add cross origin configuration

--- src/main/java/no/data/indexes/IndexesServiceApplication.java
+++ src/main/java/no/data/indexes/IndexesServiceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins="http://localhost:4200")
 public class IndexesServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/no/data/indexes/controller/IndexController.java
+++ src/main/java/no/data/indexes/controller/IndexController.java
@@ -3,18 +3,20 @@ package no.data.indexes.controller;
 import no.data.indexes.dao.IndexRepository;
 import no.data.indexes.model.Index;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:8080")
 public class IndexController {
 
     @Autowired
     private IndexRepository repository;
 
-    @GetMapping
+    @GetMapping("/api/index")
     public List<Index> getIndexes(){
         return (List<Index>) repository.findAll();
     }
